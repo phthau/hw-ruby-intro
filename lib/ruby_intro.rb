2861657ec20ad70b15f6ff421bf06b0e0690c651
EC2 Default User <ec2-user@ip-172-31-0-149.us-east-2.compute.internal>
2021-02-08 21:54:32 +0000

    Finish part 2.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -18,23 +18,24 @@ def sum_to_n? arr, n
       return true
     end
   end
-  false  
-  # puts arr2.length()
-  # YOUR CODE HERE
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^([bcdfghjklmnpqrstvwxys])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[01]+$/
+    return s.to_i % 4 == 0
+  end
+  false
 end
 
 # Part 3
