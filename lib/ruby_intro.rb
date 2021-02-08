658778c0cbcd19c741138a028456652b2550ab61
EC2 Default User <ec2-user@ip-172-31-0-149.us-east-2.compute.internal>
2021-02-08 21:06:01 +0000

    Finish part 1.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,14 +3,23 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  max_2 = arr.max(2)
+  max_2.inject(0, :+)
 end
 
 def sum_to_n? arr, n
+  arr2 = arr.combination(2).to_a
+  arr2.each do |pair|
+    if pair.inject(0, :+) == n
+      return true
+    end
+  end
+  false  
+  # puts arr2.length()
   # YOUR CODE HERE
 end
 
